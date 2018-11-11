ca49ea1af7818bbdcc393cdfc600b4fef21e2b37
Goran Mekić <meka@tilda.center>
2018-11-11 02:57:20 +0100

    Rename application

--- bin/devel.sh
+++ bin/devel.sh
@@ -6,9 +6,9 @@ export PROJECT_ROOT=`readlink -f "${BIN_DIR}/.."`
 
 
 if [ -f /usr/local/bin/cbsd ]; then
-  sudo cbsd jexec user=devel jname=backend /usr/src/bin/default_user.sh
-  sudo tmux new-session -s "tilda" -d "cbsd jexec user=devel jname=backend /usr/src/bin/devel.sh"
-  sudo tmux split-window -h -p 50 -t 0 "cbsd jexec user=devel jname=frontend /usr/src/bin/devel.sh"
+  sudo cbsd jexec user=devel jname=pyserback /usr/src/bin/default_user.sh
+  sudo tmux new-session -s "tilda" -d "cbsd jexec user=devel jname=pyserback /usr/src/bin/devel.sh"
+  sudo tmux split-window -h -p 50 -t 0 "cbsd jexec user=devel jname=pyserfront /usr/src/bin/devel.sh"
   sudo tmux a
 else
   "${PROJECT_ROOT}/services/backend/bin/default_user.sh"
