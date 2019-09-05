278cbe2202b3273168a7def9a9a2785c95845984
Goran Mekić <meka@tilda.center>
2019-09-05 23:43:13 +0200

    Upgrade to newer stack

--- bin/devel.sh
+++ bin/devel.sh
@@ -8,17 +8,15 @@ export OFFLINE="${OFFLINE:=no}"
 
 if [ -f /usr/local/bin/cbsd ]; then
   backend_hostname=$(sudo cbsd jexec user=devel jname=pyserback hostname)
-  echo "HTTP_PROXY=http://${backend_hostname}:5000" >"${PROJECT_ROOT}/services/frontend/project.conf"
-  sudo cbsd jexec user=devel jname=pyserback env OFFLINE=${OFFLINE} /usr/src/bin/default_user.sh
+  sudo cbsd jexec user=devel jname=pyserback env OFFLINE=${OFFLINE} /usr/src/bin/init.sh
   sudo tmux new-session -s "pyser" -d "cbsd jexec user=devel jname=pyserback env OFFLINE=${OFFLINE} /usr/src/bin/devel.sh"
-  sudo tmux split-window -h -p 50 -t 0 "cbsd jexec user=devel jname=pyserfront env OFFLINE=${OFFLINE} /usr/src/bin/devel.sh"
+  sudo tmux split-window -h -p 50 -t 0 "sudo cbsd jexec user=devel jname=pyserfront env OFFLINE=${OFFLINE} BACKEND_URL=\"http://${backend_hostname}:5000\" /usr/src/bin/devel.sh"
   sudo tmux a -t "pyser"
 else
   backend_hostname='localhost:5000'
-  echo "HTTP_PROXY=http://${backend_hostname}:5000" >"${PROJECT_ROOT}/services/frontend/project.conf"
   "${PROJECT_ROOT}/bin/download_repos.sh"
-  "env OFFLINE=${OFFLINE} ${PROJECT_ROOT}/services/backend/bin/default_user.sh"
+  env OFFLINE=${OFFLINE} "${PROJECT_ROOT}/services/backend/bin/init.sh"
   tmux new-session -s "pyser" -d "env OFFLINE=${OFFLINE} ${PROJECT_ROOT}/services/backend/bin/devel.sh"
-  tmux split-window -h -p 50 -t 0 "env OFFLINE=${OFFLINE} ${PROJECT_ROOT}/services/frontend/bin/devel.sh"
+  tmux split-window -h -p 50 -t 0 "env OFFLINE=${OFFLINE} BACKEND_URL=\"http://${backend_hostname}:5000\" ${PROJECT_ROOT}/services/frontend/bin/devel.sh"
   tmux a -t "pyser"
 fi
